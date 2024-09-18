caecd18a86649df5b54413dfe31ae741bcbbee5f
Ailer-h <henriqueailer2@gmail.com>
2024-09-18 14:42:03 -0300

    Commit Inicial

---
+++ php/login.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+  <title>Login</title>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body class="p-0 m-0">
+
+  <form action="">
+    <div class="d-flex justify-content-md-center align-items-center vh-100">
+      <div class="row">
+        <div class="col">
+          <div class="container bg-primary-subtle p-1">
+            <div class="form-floating mb-3">
+              <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+              <label for="floatingInput">Email address</label>
+              <div class="invalid-feedback">
+        Please choose a username.
+          </div>
+            </div>
+            <div class="form-floating">
+              <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+              <label for="floatingPassword">Password</label>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </form>
+</body>
+
+</html>
