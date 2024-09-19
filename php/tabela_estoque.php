c8261736a2bf06dbc066443c5b0dde3458213e71
Ailer-h <henriqueailer2@gmail.com>
2024-09-19 10:31:56 -0300

    Finalizado o header da tabela

--- php/tabela_estoque.php
+++ php/tabela_estoque.php
@@ -7,7 +7,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/style.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <title>Document</title>
+    <script src="https://cdn.jsdelivr.net/npm/cleave.js@1.6.0/dist/cleave.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <title>Estoque</title>
 </head>
 
 <body class="bg-body-tertiary h-100">
@@ -37,12 +39,12 @@
     </nav>
 
     <div class="container text-center mt-3">
-        <div class="row align-items-center">
+        <div class="row align-items-center border-bottom w-100 pb-2">
             <div class="col">
                 <h1>Estoque</h1>
             </div>
             <div class="col">
-                <input class="form-control" type="text" placeholder="dd/mm/aaaa" aria-label="default input example">
+                <input class="form-control" type="text" placeholder="dd/mm/aaaa" name="filter-date" id="filter-date" aria-label="default input example">
             </div>
             <div class="col">
                 <select class="form-select" aria-label="Default select example">
@@ -52,20 +54,30 @@
                     <option value="3">Em estoque</option>
                 </select>
             </div>
-            <div class="col"></div>
-            <div class="col"></div>
+            <div class="col">
+                <button type="button" class="btn btn-warning fw-bold">
+                    <i class="bi bi-plus-circle"></i>
+                    Novo item
+                </button>
+            </div>
         </div>
     </div>
 
 
     <footer class="bg-body-tertiary text-center text-lg-start">
-        <!-- Copyright -->
         <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05); font-size: .75em;">
             by Henrique Ailer & Murilo Martins. <br>
             <a class="text-body" style="font-size: .75em;" href="https://github.com/Ailer-h/erp_logistica">github.com</a>
         </div>
-        <!-- Copyright -->
     </footer>
 </body>
+<script>
+    //Adicionando a mascara na data
+    new Cleave('#filter-date', {
+        date: true,
+        delimiter: "/",
+        datePattern: ['d', 'm', 'Y']
+    });
+</script>
 
 </html>
