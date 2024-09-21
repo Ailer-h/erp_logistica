b2ce2bf98cc4b40647300a2dfb5a2c4f1496c262
murilo-martin <murilomendonca967@gmail.com>
2024-09-20 21:09:18 -0300

    Edit com modal iniciado

--- js/estoque.js
+++ js/estoque.js
@@ -1,5 +1,6 @@
 const forms = document.querySelectorAll('.needs-validation')
 const modal_add = new bootstrap.Modal('#modal_add');
+const modal_edit = new bootstrap.Modal('#modal_edit');
 
 document.addEventListener('DOMContentLoaded', function(){
     table('searchbar');
@@ -37,7 +38,7 @@ document.getElementById('form_add').addEventListener('submit', event => {
             success: function(){
                 modal_add.hide();
                 clear_form();
-                table('');
+                table('searchbar');
             }
         })
     }
@@ -94,4 +95,31 @@ function table(searchbar_id){
             $('#table').html(data)
         }
     });
+}
+function showEditModal(id_materiaPrima){
+    
+    modal_edit.show();
+
+    $.ajax({
+
+        // Chama o arquivo que pega as informações
+        url: "utilities/puxarInfo_estoque.php",
+        // Define o method
+        method: "post",
+        // Valores de "input"
+        data:{
+
+            id: id_materiaPrima
+
+        },
+        // Quando der certo retorna um valor
+        success: function(data){
+
+            $("#modalEdit_body").html(data);
+
+        }
+
+    });
+
+
 }
--- php/tabela_estoque.php
+++ php/tabela_estoque.php
@@ -149,6 +149,26 @@
             </div>
         </div>
     </div>
+    <!-- Modal Edit -->
+    <div class="modal fade" id="modal_edit" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Novo Item</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" onclick="clear_form()"></button>
+                </div>
+                <form id="form_add" class="needs-validation" novalidate>
+                    <div class="modal-body" id="modalEdit_body">
+                        
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" onclick="clear_form()">Cancelar</button>
+                        <button type="submit" class="btn btn-warning">Salvar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 
     <footer class="bg-body-tertiary text-center text-lg-start">
         <div class="text-center p-2" style="background-color: rgba(0, 0, 0, 0.05); font-size: .75em;">
