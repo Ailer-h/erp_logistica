93a4b516135695f0cf5ee5c7bc13039842c5d8b8
Henrique Ailer <129389768+Ailer-h@users.noreply.github.com>
2024-10-08 08:54:10 -0300

    Conserto do database.

--- erp_logistica.sql
+++ erp_logistica.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Sep 21, 2024 at 04:14 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Host: 127.0.0.1:3306
+-- Tempo de geração: 08-Out-2024 às 11:53
+-- Versão do servidor: 8.0.31
+-- versão do PHP: 8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,154 +18,95 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `erp_logistica`
+-- Banco de dados: `erp_logistica`
 --
-CREATE DATABASE IF NOT EXISTS `erp_logistica` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE IF NOT EXISTS `erp_logistica` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 USE `erp_logistica`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `empresa`
+-- Estrutura da tabela `empresa`
 --
 
-CREATE TABLE `empresa` (
-  `id_empresa` int(11) NOT NULL,
+DROP TABLE IF EXISTS `empresa`;
+CREATE TABLE IF NOT EXISTS `empresa` (
+  `id_empresa` int NOT NULL AUTO_INCREMENT,
   `nome_empresa` varchar(50) NOT NULL,
   `email_empresa` varchar(50) NOT NULL,
-  `senha_empresa` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `senha_empresa` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_empresa`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `empresa`
+-- Extraindo dados da tabela `empresa`
 --
 
 INSERT INTO `empresa` (`id_empresa`, `nome_empresa`, `email_empresa`, `senha_empresa`) VALUES
-(1, 'Admin', 'adm@adm.com', 'hm_acesso123');
-(2, 'Camisetas', 'camisas@empresa.com', 'camiseta_acesso');
-(3, 'Jeans', 'jeans@empresa.com', 'jeans_acesso');
-(4, 'Paçoca', 'pacoca@empresa.com', 'pacoca_acesso');
-(5, 'Macarrão', 'macarrao@empresa.com', 'macarrao_acesso');
-(6, 'Tenis', 'tenis@empresa.com', 'tenis_acesso');
+(1, 'Admin', 'adm@adm.com', 'hm_acesso123'),
+(2, 'Camisetas', 'camisas@empresa.com', 'camiseta_acesso'),
+(3, 'Jeans', 'jeans@empresa.com', 'jeans_acesso'),
+(4, 'Paçoca', 'pacoca@empresa.com', 'pacoca_acesso'),
+(5, 'Macarrão', 'macarrao@empresa.com', 'macarrao_acesso'),
+(6, 'Tenis', 'tenis@empresa.com', 'tenis_acesso'),
 (7, 'Brigadeiro', 'brigadeiro@empresa.com', 'brigadeiro_acesso');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `estoque`
+-- Estrutura da tabela `estoque`
 --
 
-CREATE TABLE `estoque` (
-  `id_materiaPrima` int(11) NOT NULL,
-  `id_empresa` int(11) NOT NULL,
+DROP TABLE IF EXISTS `estoque`;
+CREATE TABLE IF NOT EXISTS `estoque` (
+  `id_materiaPrima` int NOT NULL AUTO_INCREMENT,
+  `id_empresa` int NOT NULL,
   `nome_produto` varchar(100) NOT NULL,
-  `qtd_padrao` int(11) NOT NULL,
-  `qtd_estoque` int(11) NOT NULL DEFAULT 0,
+  `qtd_padrao` int NOT NULL,
+  `qtd_estoque` int NOT NULL DEFAULT '0',
   `unidade_medida` varchar(10) NOT NULL,
-  `estado` varchar(10) NOT NULL DEFAULT 'em_uso'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `estado` varchar(10) NOT NULL DEFAULT 'em_uso',
+  PRIMARY KEY (`id_materiaPrima`),
+  KEY `id_empresa` (`id_empresa`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `nota`
+-- Estrutura da tabela `nota`
 --
 
-CREATE TABLE `nota` (
-  `id_nota` int(11) NOT NULL,
-  `id_empresa` int(11) NOT NULL,
-  `produto_nota` int(11) NOT NULL,
-  `qtd_produto` int(11) NOT NULL,
+DROP TABLE IF EXISTS `nota`;
+CREATE TABLE IF NOT EXISTS `nota` (
+  `id_nota` int NOT NULL AUTO_INCREMENT,
+  `id_empresa` int NOT NULL,
+  `produto_nota` int NOT NULL,
+  `qtd_produto` int NOT NULL,
   `data_nota` date DEFAULT NULL,
-  `estado_nota` varchar(15) NOT NULL DEFAULT 'Requisitada'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `estado_nota` varchar(15) NOT NULL DEFAULT 'Requisitada',
+  PRIMARY KEY (`id_nota`),
+  KEY `id_empresa` (`id_empresa`),
+  KEY `produto_nota` (`produto_nota`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `registro`
+-- Estrutura da tabela `registro`
 --
 
-CREATE TABLE `registro` (
-  `id_registro` int(11) NOT NULL,
-  `id_empresa` int(11) NOT NULL,
-  `id_nota` int(11) NOT NULL,
-  `qtd_recebida` int(11) DEFAULT NULL,
+DROP TABLE IF EXISTS `registro`;
+CREATE TABLE IF NOT EXISTS `registro` (
+  `id_registro` int NOT NULL AUTO_INCREMENT,
+  `id_empresa` int NOT NULL,
+  `id_nota` int NOT NULL,
+  `qtd_recebida` int DEFAULT NULL,
   `data_registro` date DEFAULT NULL,
-  `estado_registro` varchar(20) NOT NULL DEFAULT 'Requisitado'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `empresa`
---
-ALTER TABLE `empresa`
-  ADD PRIMARY KEY (`id_empresa`);
-
---
--- Indexes for table `estoque`
---
-ALTER TABLE `estoque`
-  ADD PRIMARY KEY (`id_materiaPrima`),
-  ADD KEY `id_empresa` (`id_empresa`);
-
---
--- Indexes for table `nota`
---
-ALTER TABLE `nota`
-  ADD PRIMARY KEY (`id_nota`),
-  ADD KEY `id_empresa` (`id_empresa`),
-  ADD KEY `produto_nota` (`produto_nota`);
-
---
--- Indexes for table `registro`
---
-ALTER TABLE `registro`
-  ADD PRIMARY KEY (`id_registro`),
-  ADD KEY `id_nota` (`id_nota`),
-  ADD KEY `id_empresa` (`id_empresa`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `empresa`
---
-ALTER TABLE `empresa`
-  MODIFY `id_empresa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT for table `estoque`
---
-ALTER TABLE `estoque`
-  MODIFY `id_materiaPrima` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `nota`
---
-ALTER TABLE `nota`
-  MODIFY `id_nota` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `registro`
---
-ALTER TABLE `registro`
-  MODIFY `id_registro` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `nota`
---
-ALTER TABLE `nota`
-  ADD CONSTRAINT `nota_ibfk_1` FOREIGN KEY (`id_empresa`) REFERENCES `empresa` (`id_empresa`),
-  ADD CONSTRAINT `nota_ibfk_2` FOREIGN KEY (`produto_nota`) REFERENCES `estoque` (`id_materiaPrima`);
+  `estado_registro` varchar(20) NOT NULL DEFAULT 'Requisitado',
+  PRIMARY KEY (`id_registro`),
+  KEY `id_empresa` (`id_empresa`),
+  KEY `id_nota` (`id_nota`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
