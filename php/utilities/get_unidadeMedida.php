8d672db150f9c3e8c1a1a892bfece3af0e4a9a7e
Henrique Ailer <129389768+Ailer-h@users.noreply.github.com>
2024-10-08 08:56:08 -0300

    Implementação da abreviação de "unidade".

--- php/utilities/get_unidadeMedida.php
+++ php/utilities/get_unidadeMedida.php
@@ -6,7 +6,9 @@
 
     $infos = mysqli_fetch_array(mysqli_query($conexao, "select unidade_medida, qtd_estoque, qtd_padrao from estoque where id_materiaPrima = $id"));
     $qtd_notas = mysqli_fetch_array(mysqli_query($conexao, "SELECT SUM(qtd_produto) FROM nota WHERE produto_nota = $id AND estado_nota = 'Requisitada';"))[0];
-    echo $infos[0];
+
+    $unidade_medida = $infos[0] == "unidades" ? "uni." : $infos[0];
+    echo $unidade_medida;
 
     $max_nota = $infos[2] - $infos[1] - $qtd_notas > 0 ? $infos[2] - $infos[1] - $qtd_notas: 0;
 
